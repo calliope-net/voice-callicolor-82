16d26070f3ee504a8c215317a67d50f83a234c13
calliope-net <141074597+calliope-net@users.noreply.github.com>
2025-12-06 12:12:08 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ README.md
@@ -0,0 +1,50 @@
+
+> Diese Seite bei [https://calliope-net.github.io/voice-83/](https://calliope-net.github.io/voice-83/) öffnen
+
+## Als Erweiterung verwenden
+
+Dieses Repository kann als **Erweiterung** in MakeCode hinzugefügt werden.
+
+* öffne [https://makecode.calliope.cc/](https://makecode.calliope.cc/)
+* klicke auf **Neues Projekt**
+* klicke auf **Erweiterungen** unter dem Zahnrad-Menü
+* nach **https://github.com/calliope-net/voice-83** suchen und importieren
+
+## Dieses Projekt bearbeiten
+
+Um dieses Repository in MakeCode zu bearbeiten.
+
+* öffne [https://makecode.calliope.cc/](https://makecode.calliope.cc/)
+* klicke auf **Importieren** und dann auf **Importiere URL**
+* füge **https://github.com/calliope-net/voice-83** ein und klicke auf Importieren
+
+#### Metadaten (verwendet für Suche, Rendering)
+
+* for PXT/calliopemini
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
+
+
+
+> Diese Seite bei [https://calliope-net.github.io/voice-callicolor-82/](https://calliope-net.github.io/voice-callicolor-82/) öffnen
+
+## Als Erweiterung verwenden
+
+Dieses Repository kann als **Erweiterung** in MakeCode hinzugefügt werden.
+
+* öffne [https://makecode.calliope.cc/](https://makecode.calliope.cc/)
+* klicke auf **Neues Projekt**
+* klicke auf **Erweiterungen** unter dem Zahnrad-Menü
+* nach **https://github.com/calliope-net/voice-callicolor-82** suchen und importieren
+
+## Dieses Projekt bearbeiten
+
+Um dieses Repository in MakeCode zu bearbeiten.
+
+* öffne [https://makecode.calliope.cc/](https://makecode.calliope.cc/)
+* klicke auf **Importieren** und dann auf **Importiere URL**
+* füge **https://github.com/calliope-net/voice-callicolor-82** ein und klicke auf Importieren
+
+#### Metadaten (verwendet für Suche, Rendering)
+
+* for PXT/calliopemini
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,95 @@
+function macheEtwas (id: number) {
+    if (id == 45) {
+        basic.clearScreen()
+    } else if (id >= 52 && id <= 61) {
+        pins.comment(pins.pins_text("Display number 0-9"))
+        basic.showNumber(id - 52)
+    } else if (id == 62) {
+        basic.showIcon(IconNames.Happy)
+    } else if (id == 63) {
+        basic.showIcon(IconNames.Sad)
+    } else if (id == 64) {
+        basic.showIcon(IconNames.Heart)
+    } else if (id == 65) {
+        basic.clearScreen()
+    } else if (id == 69) {
+        basic.showNumber(input.temperature())
+    } else if (id == 82) {
+        control.reset()
+    } else if (id == 116) {
+        basic.setLedColor(0xff0000)
+    } else if (id == 123) {
+        basic.setLedColor(0xffffff)
+    } else {
+        basic.turnRgbLedOff()
+    }
+}
+function GitHub () {
+    pins.comment(pins.pins_text("calliope-net/voice-callicolor-82"))
+    pins.comment(pins.pins_text("CalliColor, I2C: Voice, OLED, EEPROM"))
+    pins.comment(pins.pins_text("3 Erweiterungen laden:"))
+    pins.comment(pins.pins_text("calliope-net/pins; calliope-net/matrix"))
+    pins.comment(pins.pins_text("mkleinsb/pxt-callicolor"))
+    pins.comment(pins.pins_text("Anleitung:"))
+    pins.comment(pins.pins_text("elssner.github.io/ft-Controller-I2C/voice_konsole/"))
+}
+function Zeile1_7 (text: string) {
+    matrix.clearMatrix(1, 7)
+    zeilen = Math.trunc(text.length / 16)
+    for (let Index = 0; Index <= zeilen; Index++) {
+        matrix.writeTextEEPROM(Index + 1, 0, matrix.matrix_text(text.substr(Index * 16, 16)))
+    }
+    matrix.displayMatrix(1, 7, matrix.eI2C.I2C_x3C)
+}
+function Zeile0 (zahl: number) {
+    matrix.clearMatrix(0, 0)
+    matrix.writeTextEEPROM(0, 0, zahl)
+    matrix.displayMatrix(0, 0, matrix.eI2C.I2C_x3C)
+}
+function callicolor (id: number) {
+    if (id == 116) {
+        CalliColor.showCalliColor(0xff0000)
+    } else if (id == 117) {
+        CalliColor.showCalliColor(0xffaa00)
+    } else if (id == 118) {
+        CalliColor.showCalliColor(0xffff00)
+    } else if (id == 119) {
+        CalliColor.showCalliColor(0x00ff00)
+    } else if (id == 120) {
+        CalliColor.showCalliColor(0x00ffff)
+    } else if (id == 121) {
+        CalliColor.showCalliColor(0x0000ff)
+    } else if (id == 122) {
+        CalliColor.showCalliColor(0xff00ff)
+    } else if (id == 123) {
+        CalliColor.showCalliColor(0xffffff)
+    } else {
+        CalliColor.showCalliColor(0x000000)
+    }
+}
+let Kommando_ID = 0
+let zeilen = 0
+matrix.init(matrix.ePages.y64)
+matrix.displayMatrix()
+basic.pause(2000)
+let wachzeit = pins.voice_register(pins.pins_voice_eRegister(pins.voice_eRegister.WAKE_TIME))
+Zeile0(wachzeit)
+let connected = wachzeit > 0
+if (connected) {
+    basic.setLedColor(0x00ff00)
+} else {
+    basic.setLedColor(0xff0000)
+}
+basic.forever(function () {
+    if (connected) {
+        Kommando_ID = pins.voice_read_cmdid()
+        if (Kommando_ID == 0) {
+            basic.pause(2000)
+        } else {
+            Zeile0(Kommando_ID)
+            macheEtwas(Kommando_ID)
+            callicolor(Kommando_ID)
+            Zeile1_7(pins.voice_command_text(Kommando_ID))
+        }
+    }
+})
